5a663269330b1350a6393f5b68c694469c34fbfd
jackie jan <zakaria.farah55@outlook.com>
2025-07-29 12:59:07 +0300

    added working implementation of arena memory management

---
+++ src/arena_allocator/arena.c
@@ -0,0 +1,51 @@
+#include "minishell.h"
+#include <string.h>
+
+t_arena *init_arena(size_t size)
+{
+    t_arena *arena;
+
+    arena = malloc(sizeof(t_arena));
+    if (!arena)
+        return (NULL);
+    arena->size = size;
+    arena->offset = 0;
+    arena->next = NULL;
+    arena->buffer = malloc(arena->size);
+    arena->alloc = arena_alloc;
+    arena->destroy = destroy_arena;
+    if (!arena->buffer)
+    {
+        free(arena);
+        return (NULL);
+    }
+    return (arena);
+}
+
+void *arena_alloc(t_arena *arena, size_t size, void *content)
+{
+    t_arena *tail;
+    size_t prev_off;
+    size_t new_off;
+
+    tail = tail_arena(arena);
+    new_off = allign_p(tail, size);
+    if (tail->size - new_off > size)
+    {
+        prev_off = arena->offset;
+        tail->offset = new_off;
+        ft_memcpy(tail->buffer + prev_off, content, size);
+        return (tail->buffer + prev_off);
+    }
+    if (size > ARENA_SIZE)
+        tail->next = init_arena(size);
+    else
+        tail->next = init_arena(ARENA_SIZE);
+    if (!tail->next)
+        return (NULL);
+    tail = tail->next;
+    prev_off = tail->offset;
+    tail->offset += size;
+    ft_memcpy(tail->buffer + prev_off, content, size);
+    return (tail->buffer + prev_off);
+}
