7ce990d9de27b7e3112b39295ef3fc292d36307a
jackie jan <zakaria.farah55@outlook.com>
2025-07-29 12:58:37 +0300

    arena func utils

---
+++ src/arena_allocator/arena_utils.c
@@ -0,0 +1,37 @@
+#include "arena.h"
+
+t_arena *tail_arena(t_arena *arena)
+{
+    while (arena)
+    {
+        if (!arena->next)
+            return (arena);
+        arena = arena->next;
+    }
+    return NULL;
+}
+
+void destroy_arena(t_arena *arena)
+{
+    t_arena *tmp;
+
+    while (arena->next)
+    {
+        free(arena->buffer);
+        tmp = arena;
+        arena->next = arena->next;
+        free(tmp);
+    }
+    free(arena->buffer);
+    free(arena);
+}
+
+size_t allign_p(t_arena *arena, size_t size)
+{
+    const size_t modulo = (arena->offset) % ALIGNMENT;
+
+    ft_printf("Size: %d, off: %d, modulo: %d\n", size, arena->offset, modulo);
+    if (size > modulo && modulo != 0)
+        return arena->offset + ALIGNMENT - modulo + size;
+    return arena->offset + size;
+}
