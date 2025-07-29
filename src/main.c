506d338e0d76e23ec278c8e813b0854083159b0a
Zakaria Farah <zakaria.farah55@outlook.com>
2025-07-29 18:21:15 +0300

    added main loop that read user prompt

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 # include "minishell.h"
 
-int main(void)
+int main(int argc, char **args)
 {
+    const t_arena *arena = init_arena(ARENA_SIZE);
+    char *prompt;
+
+    if (!arena)
+        exit(1);
+    while (true)
+    {
+        prompt = read_prompt();
+        if (!prompt)
+            break;
+        free(prompt);
+    }
+    clean_up(arena);
     return (0);
 }
