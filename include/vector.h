30070c09e88027d53bb45667a1e2c6da0408ee7b
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-05 10:40:10 +0300

    added vector header file

---
+++ include/vector.h
@@ -0,0 +1,32 @@
+# ifndef VECTORS_H
+# define VECTORS_H
+
+#include <stdlib.h>
+#include "minishell.h"
+
+typedef struct s_vector t_vector;
+
+typedef struct s_vector
+{
+    int size;
+    int capacity;
+    size_t elem_size;
+    void *data;
+    void (*pop)(t_vector *vector);
+    void *(*push)(t_vector *vector, void *content);
+    void *(*get)(t_vector *vector, int index);
+    void  (*free)(t_vector *vector);
+    void  (*clear)(t_vector *vector);
+} t_vector;
+
+
+t_vector *init_vector(int capacity, size_t elem_size, void *content);
+
+// methods
+void    *append(t_vector *vector, void *content);
+void    *get_vector_elem(t_vector *vector, int index);
+void    pop(t_vector *vector);
+void    free_vector(t_vector *vector);
+void    clear_vector(t_vector *vector);
+
+# endif
