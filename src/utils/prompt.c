c0d0575f236e83db7bf82f901a495803118e20e4
nraatika <nraatika@student.hive.fi>
2025-08-08 11:07:42 +0300

    changed read_prompt to move the received string to arena and free the one malloced by readline

--- src/utils/prompt.c
+++ src/utils/prompt.c
@@ -1,12 +1,15 @@
 #include "minishell.h"
 
-char *read_prompt(void)
+char *read_prompt(t_arena *arena)
 {
     char *prompt;
+	char *arena_prompt;
     
     prompt = readline("minishell>> ");
     if (!prompt)
         return (NULL);
-    add_history(prompt);
-    return (prompt);
+	arena_prompt = arena_alloc(arena, ft_strlen(prompt) + 1, prompt);
+	free(prompt);
+    add_history(arena_prompt);
+    return (arena_prompt);
 }
