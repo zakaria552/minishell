c5c1cfa1d2a34e7a23bc58bb83b11b213fd97872
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-11 17:17:01 +0300

    wip runtime_err handling

--- src/execution/error_handler.c
+++ src/execution/error_handler.c
@@ -2,6 +2,7 @@
 
 void    runtime_err(char *msg)
 {
+    t_allocators *allocs = get_allocators();
     ft_putstr_fd("minishell:", 2);
     if (msg)
     {
@@ -10,5 +11,7 @@ void    runtime_err(char *msg)
     }
     ft_putstr_fd(strerror(errno), 2);
     ft_putstr_fd("\n", 2);
+    clean_up(allocs->global, true);
+    clean_up(allocs->tmp, false);
     exit(errno);
 }
