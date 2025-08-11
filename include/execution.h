d6222e10438da7bb7b7c24633dd0b6db1d451f5b
jackie jan <zakaria.farah55@outlook.com>
2025-08-11 09:37:17 +0300

    wip execution header

--- include/execution.h
+++ include/execution.h
@@ -4,9 +4,11 @@
 #include "minishell.h"
 
 
-void    redirect_io(t_vector *redirect);
+void    redirect_io(t_cmd *cmd);
+void    execution(t_vector *cmds, t_arena *arena, char **envp);
 
 // error handler
 void    runtime_err(char *msg);
-
+void    close_pipe(int pipe[2]);
+char	*format_path(char *command, char **envp, t_arena *arena);
 #endif
