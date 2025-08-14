82fb8d0a4e31662537ef777de1852360e1ce433c
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-14 18:34:45 +0300

    Added error handlers for syntax, run time, and command not found errors

---
+++ src/utils/error_handlers.c
@@ -0,0 +1,51 @@
+#include "minishell.h"
+
+void    clean_up(bool clean_global, bool clean_history)
+{
+    t_allocators *allocs = get_allocators();
+
+    if (allocs->global && clean_global)
+        allocs->global->destroy(allocs->global);
+    if (allocs->prompt)
+        allocs->prompt->destroy(allocs->prompt);
+    if (clean_history)
+        clear_history();
+}
+
+void    syntax_err(int err_code, char *msg)
+{
+    errno = err_code;
+    ft_putstr_fd("Minishell: ", STDERR_FILENO);
+    ft_putstr_fd(msg, STDERR_FILENO);
+    ft_putstr_fd("\n", STDERR_FILENO);
+    clean_up(false, true);
+}
+
+void    runtime_err(int err_code, char *msg)
+{
+    errno = err_code;
+    ft_putstr_fd("minishell: ", 2);
+    if (msg)
+    {
+        ft_putstr_fd(msg, 2);
+        ft_putstr_fd(":", 2);
+    }
+    ft_putstr_fd(strerror(errno), 2);
+    ft_putstr_fd("\n", 2);
+    clean_up(true, true);
+    exit(errno);
+}
+
+void    cmd_not_found_err(int err_code, char *cmd, bool path_exist)
+{
+    if (err_code != 127)
+        runtime_err(errno, cmd);
+    ft_putstr_fd("minishell: ", 2);
+    ft_putstr_fd(cmd, 2);
+    ft_putstr_fd(":", 2);
+    if (path_exist)
+        ft_putstr_fd("command not found", 2);
+    else
+        ft_putstr_fd("No such file or directory", 2);
+    clean_up(true, true);
+}
