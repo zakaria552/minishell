4328447dd28255ba0cfe42c0e785f8288e90a9eb
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-15 11:34:55 +0300

    ft_error with syntax errr

--- src/parsing/tokenize.c
+++ src/parsing/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 13:27:39 by nraatika          #+#    #+#             */
-/*   Updated: 2025/08/15 14:04:22 by nraatika         ###   ########.fr       */
+/*   Updated: 2025/08/15 14:07:20 by nraatika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -133,7 +133,7 @@ t_vector	*tokenize_input(char *s, t_arena *arena, char delimiter)
 		if (tok->read_chars > 0)
 			s += tok->read_chars;
 		else 
-			clean_exit(arena, 3, "token length < 1");
+			syntax_err(3, "token length < 1");
 	}
 	return (vec);
 }
