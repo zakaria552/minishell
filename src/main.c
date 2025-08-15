a49edd74ad3290fa2010544cba5f7081ccdd5975
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-15 10:48:03 +0300

    added allocater strucure that contains global and prompt arenas

--- src/main.c
+++ src/main.c
@@ -2,51 +2,41 @@
 
 int main(int argc, char **args, char **envp)
 {
-  t_arena *arena;
-  char *prompt;
-  t_vector *vec;
-  t_vector *commands;
-  int i;
+    t_allocators *allocs = get_allocators();
+    char *prompt;
+    t_vector *vec;
+    t_vector *commands;
+    int i;
 
-  (void)argc;
-  (void)args;
-  while (true)
-  {
-    arena = init_arena(ARENA_SIZE);
-    if (!arena)
-      exit(1);
-    prompt = read_prompt(arena);
-    if (!prompt)
-      break;
-    vec = tokenize_input(prompt, arena, '\0');
-    i = -1;
-    while (++i < vec->size)
-      print_token((t_token *)vec->get(vec, i));
-    commands = parse_tokens_to_commands(arena, vec);
-    i = -1;
-    while (++i < commands->size)
-      print_command((t_cmd *)commands->get(commands, i));
-    // todos: error handling, cleaning up the heap, refactor, global arena
-    handle_here_doc(commands);
-    execution(commands, arena, envp);
-    clean_up((t_arena *)arena, false);
-  }
-  return (0);
-}
-
-t_arena *get_allocator()
-{
-  static t_arena *arena;
-  if (!arena)
-    arena = init_arena(ARENA_SIZE);
-  return (arena);
+    (void)argc;
+    (void)args;
+    while (true)
+    {
+        allocs->prompt = init_arena(ARENA_SIZE);
+        prompt = read_prompt("minishell> ", true);
+        if (!prompt)
+            break;
+        vec = tokenize_input(prompt, allocs->prompt, '\0');
+        // i = -1;
+        // while (++i < vec->size)
+        //     print_token((t_token *)vec->get(vec, i));
+        commands = parse_tokens_to_commands(allocs->prompt, vec);
+        // i = -1;
+        // while (++i < commands->size)
+        //     print_command((t_cmd *)commands->get(commands, i));
+        // todos: error handling, cleaning up the heap, refactor, global arena
+        handle_here_doc(commands);
+        execution(commands, allocs->prompt, envp);
+        clean_up(false, false);
+    }
+    return (0);
 }
 
 t_allocators *get_allocators()
 {
-  static t_allocators arenas;
+    static t_allocators arenas;
 
-  if (arenas.global)
-    arenas.global = init_arena(ARENA_SIZE);
-  return (&arenas);
+    if (arenas.global)
+        arenas.global = init_arena(ARENA_SIZE);
+    return (&arenas);
 }
