0a63240d4c3610ef0f54e7edbd71499664ca5080
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-15 11:35:20 +0300

    added line at the end

--- src/utils/error_handlers.c
+++ src/utils/error_handlers.c
@@ -44,8 +44,8 @@ void    cmd_not_found_err(int err_code, char *cmd, bool path_exist)
     ft_putstr_fd(cmd, 2);
     ft_putstr_fd(":", 2);
     if (path_exist)
-        ft_putstr_fd("command not found", 2);
+        ft_putstr_fd("command not found\n", 2);
     else
-        ft_putstr_fd("No such file or directory", 2);
+        ft_putstr_fd("No such file or directory\n", 2);
     clean_up(true, true);
 }
