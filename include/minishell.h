1917ab0f4c10ea010b4e52332f302c274b576d3f
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-15 10:48:03 +0300

    added allocater strucure that contains global and prompt arenas

--- include/minishell.h
+++ include/minishell.h
@@ -21,10 +21,9 @@
 typedef struct s_allocators
 {
     t_arena *global;
-    t_arena *tmp;
+    t_arena *prompt;
 } t_allocators;
 
-t_arena *get_allocator();
 t_allocators *get_allocators();
 
 #endif
