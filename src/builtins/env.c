723ba38c9cca22bd5964ccf6012c7fb6ea4d1063
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-19 13:58:15 +0300

    added env builtin func

---
+++ src/builtins/env.c
@@ -0,0 +1,17 @@
+#include "minishell.h"
+
+void    env(void)
+{
+    t_local_vars *vars;
+    t_env_var *var;
+    int i;
+
+    i = -1;
+    vars = get_local_vars();
+    while (++i < vars->envp->size)
+    {
+        var = vars->envp->get(vars->envp, i);
+        ft_printf("%s\n", var->joint);
+    }
+    vars->status = 0;
+}
