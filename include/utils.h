c5e3a151d529565afac7e4b1bd13c54a4490f9fb
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-19 14:09:43 +0300

    added func that expands string that contain multiple env vars

--- include/utils.h
+++ include/utils.h
@@ -1,7 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include "arena.h"
+#include "minishell.h"
 
 // error handlers
 void    runtime_err(int err_code, char *msg);
@@ -12,4 +12,6 @@ void    cmd_not_found_err(int err_code, char *cmd, bool path_exist);
 //  prompt utils
 char *read_prompt(char *prompt_msg, bool update_history);
 
+// expand string
+char *expand_str(char *str, t_arena *arena);
 #endif
