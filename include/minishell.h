6ddf54c8642e2d64039897f5b0d8b0bb8d5796d7
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-21 21:06:13 +0300

    prototypes for minishell

--- include/minishell.h
+++ include/minishell.h
@@ -30,6 +30,8 @@ typedef struct s_allocators
     t_arena *prompt;
 } t_allocators;
 
+// shell
+void launch_shell(bool interactive);
 t_allocators *get_allocators();
 extern volatile sig_atomic_t	g_signal;
 
