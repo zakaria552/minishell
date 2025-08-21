97bc8602a06adefba7e231813953d7e1ea9b32fa
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-21 21:03:48 +0300

    Refactored, and now handles piping cmd to minishell

--- src/main.c
+++ src/main.c
@@ -3,39 +3,13 @@ volatile sig_atomic_t	g_signal;
 
 int main(int argc, char **args, char **envp)
 {
-    t_allocators *allocs = get_allocators();
-    char *prompt;
-    t_vector *vec;
-    t_vector *commands;
-
 	(void)argc;
 	(void)args;
+
 	init_local_vars(envp);
-	while (true)
-	{
-		allocs->prompt = init_arena(ARENA_SIZE);
-		set_readline_handler();
-		prompt = read_prompt("minishell> ", true);
-		if (!prompt)
-		   break;
-		vec = tokenize_input(prompt, allocs->prompt, '\0');
-		if (!vec)
-		   continue ;
-        commands = parse_tokens_to_commands(allocs->prompt, vec);
-		if (!commands)
-		   continue ;
-        handle_here_doc(commands);
-        execute_commands(commands, allocs->prompt);
-        clean_up(false, false);
-    }
-    return (0);
-}
-
-t_allocators *get_allocators()
-{
-	static t_allocators arenas;
-
-    if (!arenas.global)
-        arenas.global = init_arena(ARENA_SIZE);
-    return (&arenas);
+	if (isatty(STDIN_FILENO))
+		launch_shell(true);
+	else
+		launch_shell(false);
+	return (get_local_vars()->status);
 }
