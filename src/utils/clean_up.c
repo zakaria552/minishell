301759cf7674ff383c6dd9f32a8b1f5d43cd3a83
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-25 16:31:11 +0300

    norm fixes for utils

--- src/utils/clean_up.c
+++ src/utils/clean_up.c
@@ -1,10 +1,10 @@
 #include "minishell.h"
 
-void    clean_up(t_arena *arena, bool clean_history)
+void	clean_up(t_arena *arena, bool clean_history)
 {
-    arena->destroy(arena);
-    if (clean_history)
-        clear_history();
+	arena->destroy(arena);
+	if (clean_history)
+		clear_history();
 }
 
 void	ft_error(char *msg)
@@ -12,12 +12,12 @@ void	ft_error(char *msg)
 	ft_printf("minishell: %s\n", msg);
 }
 
-void    clean_exit(t_arena *arena, int err_code, char *msg)
+void	clean_exit(t_arena *arena, int err_code, char *msg)
 {
-    if (arena)
-        clean_up(arena, true);
-    errno = err_code;
-    if (msg)
-        ft_printf("minishell: %s", msg);
-    exit(errno);
+	if (arena)
+		clean_up(arena, true);
+	errno = err_code;
+	if (msg)
+		ft_printf("minishell: %s", msg);
+	exit(errno);
 }
--- src/utils/prompt.c
+++ src/utils/prompt.c
@@ -1,79 +1,81 @@
 #include "minishell.h"
 
-static char *read_prompt(char *prompt_msg, bool update_history);
-static void display_working_dir(void);
-static char *get_pwd(void);
+static char	*read_prompt(char *prompt_msg, bool update_history);
+static void	display_working_dir(void);
+static char	*get_pwd(void);
 
-char *int_tty_prompt(char *prompt_msg, bool update_history, bool interactive)
+char	*int_tty_prompt(char *prompt_msg, bool update_history, bool interactive)
 {
-	t_allocators *allocs = get_allocators();
-    char *prompt;
-	char *arena_prompt;
-	char *line;
+	t_allocators	*allocs;
+	char			*prompt;
+	char			*arena_prompt;
+	char			*line;
 
+	allocs = get_allocators();
 	if (interactive)
-    	return read_prompt(prompt_msg, update_history);
+		return (read_prompt(prompt_msg, update_history));
 	line = get_next_line(STDIN_FILENO);
 	if (!line)
-		return NULL;
+		return (NULL);
 	prompt = ft_strtrim(line, "\n");
 	if (!prompt)
 	{
 		free(line);
-		return NULL;
+		return (NULL);
 	}
 	free(line);
 	arena_prompt = arena_strdup(allocs->prompt, prompt);
 	free(prompt);
-    return (arena_prompt);
+	return (arena_prompt);
 }
 
-static char *read_prompt(char *prompt_msg, bool update_history)
+static char	*read_prompt(char *prompt_msg, bool update_history)
 {
-	t_allocators *allocs = get_allocators();
-    char *prompt;
-	char *arena_prompt;
-    
+	t_allocators	*allocs;
+	char			*prompt;
+	char			*arena_prompt;
+
+	allocs = get_allocators();
 	if (update_history)
 		display_working_dir();
-    prompt = readline(prompt_msg);
+	prompt = readline(prompt_msg);
 	if (!prompt)
 	{
 		if (errno > 0)
 			runtime_err(errno, NULL);
-		return NULL;
+		return (NULL);
 	}
 	arena_prompt = arena_alloc(allocs->prompt, ft_strlen(prompt) + 1, prompt);
 	free(prompt);
 	if (update_history)
-    	add_history(arena_prompt);
-    return (arena_prompt);
+		add_history(arena_prompt);
+	return (arena_prompt);
 }
 
-static char *get_pwd(void)
+static char	*get_pwd(void)
 {
-	const t_local_vars *vars = get_local_vars();
-	const t_env_var *home = vars->get("HOME");
-	int len;
-	int i;
+	const t_local_vars	*vars = get_local_vars();
+	const t_env_var		*home = vars->get("HOME");
+	int					len;
+	int					i;
 
 	if (!home)
-		return vars->pwd;
+		return (vars->pwd);
 	len = ft_strlen(home->value);
 	i = -1;
 	while (++i < len)
 	{
 		if (vars->pwd[i] != home->value[i])
-			return vars->pwd;
+			return (vars->pwd);
 	}
-	return arena_strjoin(get_allocators()->prompt, "~", vars->pwd + i);
+	return (arena_strjoin(get_allocators()->prompt, "~", vars->pwd + i));
 }
 
-static void display_working_dir(void)
+static void	display_working_dir(void)
 {
-	char *pwd;
-	char *working_dir;
-	char *tmp;
+	char	*pwd;
+	char	*working_dir;
+	char	*tmp;
 
 	pwd = get_pwd();
 	working_dir = ft_strrchr(pwd, '/');
