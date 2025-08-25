87e20e8eb9842102f3990903634d6942c7e32a49
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-25 16:31:01 +0300

    norm fixes for vector

--- src/vector/utils.c
+++ src/vector/utils.c
@@ -1,63 +1,37 @@
 #include "minishell.h"
 
-void    *append(t_vector *vector, void *content)
+void	*pop(t_vector *vector)
 {
-    void **tmp_data;
-
-    if (vector->size < vector->capacity)
-    {
-        *(vector->data + vector->size) = content;
-        vector->size++;
-        return (*(vector->data + vector->size));
-    }
-    tmp_data = vector->data;
-    vector->capacity = vector->capacity * 2;
-    if (!vector->arena)
-        vector->data = malloc(sizeof(void *) * vector->capacity);
-    else
-        vector->data = vector->arena->alloc(vector->arena, sizeof(void *) * vector->capacity, NULL);
-    if (!vector->data)
-        return (NULL);
-    ft_memcpy(vector->data, tmp_data, (vector->capacity / 2) * sizeof(void *));
-    *(vector->data + vector->size) = content;
-    vector->size++;
-    if (!vector->arena)
-        free(tmp_data);
-    return (vector->data + vector->size);
-}
-
-void    *pop(t_vector *vector)
-{
-    if (vector->size == 0)
-        return NULL;
-    vector->size--;
-    return (vector->get(vector, vector->size));
+	if (vector->size == 0)
+		return (NULL);
+	vector->size--;
+	return (vector->get(vector, vector->size));
 }
 
 void	remove_element(t_vector *vector, int index)
 {
 	if (index >= vector->size)
 		return ;
-	ft_memcpy(vector->data + index, vector->data + index + 1, (vector->size\
-		 - index - 1) * sizeof(void *));
+	ft_memcpy(vector->data + index, vector->data + index + 1, (vector->size
+			- index - 1) * sizeof(void *));
 	vector->size--;
 }
 
-void    *get_vector_elem(t_vector *vector, int index)
+void	*get_vector_elem(t_vector *vector, int index)
 {
-    if (index >= vector->size)
-        return (NULL);
-    return *(vector->data + index);
+	if (index >= vector->size)
+		return (NULL);
+	return (*(vector->data + index));
 }
 
-void    free_vector(t_vector *vector)
+void	free_vector(t_vector *vector)
 {
-    free(vector->data);
-    free(vector);
+	free(vector->data);
+	free(vector);
 }
 
-void    clear_vector(t_vector *vector)
+void	clear_vector(t_vector *vector)
 {
-    ft_memset(vector->data, 0, vector->size);
-    vector->size = 0;
+	ft_memset(vector->data, 0, vector->size);
+	vector->size = 0;
 }
--- src/vector/vector.c
+++ src/vector/vector.c
@@ -1,43 +1,71 @@
 #include "vector.h"
 
-static void    set_properties(t_vector *vector, int capacity, t_arena *arena);
-t_vector *init_vector(int capacity, void *content, t_arena *arena)
-{
-    t_vector *vector;
+static void	set_properties(t_vector *vector, int capacity, t_arena *arena);
 
-    if (!arena)
-        vector = malloc(sizeof(t_vector));
-    else
-        vector = arena->alloc(arena, sizeof(t_vector), NULL);
-    if (!vector)
-        return (NULL);
-    set_properties(vector, capacity, arena);
-    if (!arena)
-        vector->data = malloc(capacity * sizeof(void *));
-    else
-        vector->data = arena->alloc(arena, capacity * sizeof(void *), NULL);
-    if (!vector->data)
-    {
-        if (!arena)
-            free(vector);
-        return (NULL);
-    }
-    if (!content)
-        return (vector);
-    *vector->data = content;
-    vector->size++;
-    return (vector);
+t_vector	*init_vector(int capacity, void *content, t_arena *arena)
+{
+	t_vector	*vector;
+
+	if (!arena)
+		vector = malloc(sizeof(t_vector));
+	else
+		vector = arena->alloc(arena, sizeof(t_vector), NULL);
+	if (!vector)
+		return (NULL);
+	set_properties(vector, capacity, arena);
+	if (!arena)
+		vector->data = malloc(capacity * sizeof(void *));
+	else
+		vector->data = arena->alloc(arena, capacity * sizeof(void *), NULL);
+	if (!vector->data)
+	{
+		if (!arena)
+			free(vector);
+		return (NULL);
+	}
+	if (!content)
+		return (vector);
+	*vector->data = content;
+	vector->size++;
+	return (vector);
 }
 
-static void    set_properties(t_vector *vector, int capacity, t_arena *arena)
+static void	set_properties(t_vector *vector, int capacity, t_arena *arena)
 {
-    vector->size = 0;
-    vector->capacity = capacity;
-    vector->arena = arena;
-    vector->push = append;
-    vector->pop = pop;
-    vector->get = get_vector_elem;
-    vector->free = free_vector;
-    vector->clear = clear_vector;
-    vector->remove = remove_element;
+	vector->size = 0;
+	vector->capacity = capacity;
+	vector->arena = arena;
+	vector->push = append;
+	vector->pop = pop;
+	vector->get = get_vector_elem;
+	vector->free = free_vector;
+	vector->clear = clear_vector;
+	vector->remove = remove_element;
+}
+
+void	*append(t_vector *vector, void *content)
+{
+	void	**tmp_data;
+
+	if (vector->size < vector->capacity)
+	{
+		*(vector->data + vector->size) = content;
+		vector->size++;
+		return (*(vector->data + vector->size));
+	}
+	tmp_data = vector->data;
+	vector->capacity = vector->capacity * 2;
+	if (!vector->arena)
+		vector->data = malloc(sizeof(void *) * vector->capacity);
+	else
+		vector->data = vector->arena->alloc(vector->arena, sizeof(void *)
+				* vector->capacity, NULL);
+	if (!vector->data)
+		return (NULL);
+	ft_memcpy(vector->data, tmp_data, (vector->capacity / 2) * sizeof(void *));
+	*(vector->data + vector->size) = content;
+	vector->size++;
+	if (!vector->arena)
+		free(tmp_data);
+	return (vector->data + vector->size);
 }
