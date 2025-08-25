e71de9c190ff028cadcad61bbd7639176e19fc91
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-25 16:31:58 +0300

    norm fixes for the arena allocator

--- src/arena_allocator/arena.c
+++ src/arena_allocator/arena.c
@@ -1,54 +1,61 @@
 #include "minishell.h"
 #include <string.h>
 
-t_arena *init_arena(size_t size)
-{
-    t_arena *arena;
+static void	copy_content(void *dest, void *content, size_t size);
 
-    arena = malloc(sizeof(t_arena));
-    if (!arena)
-        runtime_err(ENOMEM, NULL);
-    arena->size = size;
-    arena->offset = 0;
-    arena->next = NULL;
-    arena->buffer = malloc(arena->size);
-    arena->alloc = arena_alloc;
-    arena->destroy = destroy_arena;
-    if (!arena->buffer)
-    {
-        free(arena);
-        runtime_err(ENOMEM, NULL);
-    }
+t_arena	*init_arena(size_t size)
+{
+	t_arena	*arena;
+
+	arena = malloc(sizeof(t_arena));
+	if (!arena)
+		runtime_err(ENOMEM, NULL);
+	arena->size = size;
+	arena->offset = 0;
+	arena->next = NULL;
+	arena->buffer = malloc(arena->size);
+	arena->alloc = arena_alloc;
+	arena->destroy = destroy_arena;
+	if (!arena->buffer)
+	{
+		free(arena);
+		runtime_err(ENOMEM, NULL);
+	}
 	ft_memset(arena->buffer, 0, size);
-    return (arena);
+	return (arena);
 }
 
-void *arena_alloc(t_arena *arena, size_t size, void *content)
+void	*arena_alloc(t_arena *arena, size_t size, void *content)
 {
-    t_arena *tail;
-    size_t prev_off;
-    size_t new_off;
+	t_arena	*tail;
+	size_t	prev_off;
+	size_t	new_off;
 
-    tail = tail_arena(arena);
-    new_off = allign_p(tail, size);
-    if (new_off < tail->size)
-    {
-        prev_off = tail->offset;
-        tail->offset = new_off;
-        if (content)
-            ft_memcpy((char *)tail->buffer + prev_off, content, size);
-        return (tail->buffer + prev_off);
-    }
-    if (size > ARENA_SIZE)
-        tail->next = init_arena(size);
-    else
-        tail->next = init_arena(ARENA_SIZE);
-    if (!tail->next)
-        return (NULL);
-    tail = tail->next;
-    prev_off = tail->offset;
-    tail->offset += size;
-    if (content)
-        ft_memcpy(tail->buffer, content, size);
-    return (tail->buffer);
+	tail = tail_arena(arena);
+	new_off = allign_p(tail, size);
+	if (new_off < tail->size)
+	{
+		prev_off = tail->offset;
+		tail->offset = new_off;
+		copy_content(tail->buffer + prev_off, content, size);
+		return (tail->buffer + prev_off);
+	}
+	if (size > ARENA_SIZE)
+		tail->next = init_arena(size);
+	else
+		tail->next = init_arena(ARENA_SIZE);
+	if (!tail->next)
+		return (NULL);
+	tail = tail->next;
+	prev_off = tail->offset;
+	tail->offset += size;
+	copy_content(tail->buffer, content, size);
+	return (tail->buffer);
+}
+
+static void	copy_content(void *dest, void *content, size_t size)
+{
+	if (!content)
+		return ;
+	ft_memcpy((char *)dest, content, size);
 }
--- src/arena_allocator/arena_utils.c
+++ src/arena_allocator/arena_utils.c
@@ -1,36 +1,36 @@
 #include "arena.h"
 
-t_arena *tail_arena(t_arena *arena)
+t_arena	*tail_arena(t_arena *arena)
 {
-    while (arena)
-    {
-        if (!arena->next)
-            return (arena);
-        arena = arena->next;
-    }
-    return NULL;
+	while (arena)
+	{
+		if (!arena->next)
+			return (arena);
+		arena = arena->next;
+	}
+	return (NULL);
 }
 
-void destroy_arena(t_arena *arena)
+void	destroy_arena(t_arena *arena)
 {
-    t_arena *tmp;
+	t_arena	*tmp;
 
-    while (arena->next)
-    {
-        free(arena->buffer);
-        tmp = arena;
-        arena = arena->next;
-        free(tmp);
-    }
-    free(arena->buffer);
-    free(arena);
+	while (arena->next)
+	{
+		free(arena->buffer);
+		tmp = arena;
+		arena = arena->next;
+		free(tmp);
+	}
+	free(arena->buffer);
+	free(arena);
 }
 
-size_t allign_p(t_arena *arena, size_t size)
+size_t	allign_p(t_arena *arena, size_t size)
 {
-    const size_t modulo = (arena->offset) % ALIGNMENT;
+	const size_t	modulo = (arena->offset) % ALIGNMENT;
 
-    if (size > modulo && modulo != 0)
-        return arena->offset + ALIGNMENT - modulo + size;
-    return arena->offset + size;
+	if (size > modulo && modulo != 0)
+		return (arena->offset + ALIGNMENT - modulo + size);
+	return (arena->offset + size);
 }
