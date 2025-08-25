87a32171852476c83963e34f28488815b11be88c
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-25 16:32:13 +0300

    norm fixes for execution

--- src/execution/io_redirets.c
+++ src/execution/io_redirets.c
@@ -1,106 +1,106 @@
 #include "minishell.h"
 
-static void   redirect_stdin(char *file);
-static void   redirect_stdout(t_token *redirect);
-static void     redirect_here_doc(t_cmd *cmd);
-static void     pipe_redirect(t_cmd *cmd);
+static void	redirect_stdin(char *file);
+static void	redirect_stdout(t_token *redirect);
+static void	redirect_here_doc(t_cmd *cmd);
+static void	pipe_redirect(t_cmd *cmd);
 
-void    redirect_io(t_cmd *cmd, bool redir_pipeline)
+void	redirect_io(t_cmd *cmd, bool redir_pipeline)
 {
-    t_vector *redirects;
-    t_token *redir;
-    t_token_type type;
-    int i;
-    
-    redirects = cmd->redirects;
-    i = -1;
-    if (redir_pipeline)
-        pipe_redirect(cmd);
-    redirect_here_doc(cmd);
-    while (++i < redirects->size)
-    {
-        redir = (t_token *)redirects->get(redirects, i);
-        type = redir->type;
-        if (type == INPUT_REDIR)
-            redirect_stdin(redir->content);
-        else if (type == OUTPUT_REDIR || type == OUTPUT_APPEND)
-            redirect_stdout(redir);
-    }
+	t_vector		*redirects;
+	t_token			*redir;
+	t_token_type	type;
+	int				i;
+
+	redirects = cmd->redirects;
+	i = -1;
+	if (redir_pipeline)
+		pipe_redirect(cmd);
+	redirect_here_doc(cmd);
+	while (++i < redirects->size)
+	{
+		redir = (t_token *)redirects->get(redirects, i);
+		type = redir->type;
+		if (type == INPUT_REDIR)
+			redirect_stdin(redir->content);
+		else if (type == OUTPUT_REDIR || type == OUTPUT_APPEND)
+			redirect_stdout(redir);
+	}
 }
 
-static void   redirect_stdin(char *file)
+static void	redirect_stdin(char *file)
 {
-    const int fd = open(file, O_RDONLY);
+	const int	fd = open(file, O_RDONLY);
 
-    if (fd < 0)
-        runtime_err(1, file);
-    if (dup2(fd, STDIN_FILENO) < 0)
-    {
-        close(fd);
-        runtime_err(errno, NULL);
-    }
-    close(fd);
+	if (fd < 0)
+		runtime_err(1, file);
+	if (dup2(fd, STDIN_FILENO) < 0)
+	{
+		close(fd);
+		runtime_err(errno, NULL);
+	}
+	close(fd);
 }
 
-static void   redirect_stdout(t_token *redirect)
+static void	redirect_stdout(t_token *redirect)
 {
-    int fd;
-    int flags;
-    int mode;
-    
-    flags = O_RDWR | O_CREAT;
-    mode = 0;
-    mode |= S_IRUSR | S_IWUSR;
-    mode |= S_IRGRP;
-    mode |= S_IROTH;
-    if (redirect->type == OUTPUT_APPEND)
-        flags |= O_APPEND;
-    else
-        flags |= O_TRUNC;
-    fd = open(redirect->content, flags, mode);
-    if (fd < 0)
-        runtime_err(1, NULL);
-    if (dup2(fd, STDOUT_FILENO) < 0)
-    {
-        close(fd);
-        runtime_err(errno, NULL);
-    }
-    close(fd);
+	int	fd;
+	int	flags;
+	int	mode;
+
+	flags = O_RDWR | O_CREAT;
+	mode = 0;
+	mode |= S_IRUSR | S_IWUSR;
+	mode |= S_IRGRP;
+	mode |= S_IROTH;
+	if (redirect->type == OUTPUT_APPEND)
+		flags |= O_APPEND;
+	else
+		flags |= O_TRUNC;
+	fd = open(redirect->content, flags, mode);
+	if (fd < 0)
+		runtime_err(1, NULL);
+	if (dup2(fd, STDOUT_FILENO) < 0)
+	{
+		close(fd);
+		runtime_err(errno, NULL);
+	}
+	close(fd);
 }
 
-static void     redirect_here_doc(t_cmd *cmd)
+static void	redirect_here_doc(t_cmd *cmd)
 {
-    t_token *last_redirect;
-    const int fd = cmd->fd_here_doc;
-    
-    if (cmd->redirects->size == 0 || fd < 0) 
-        return ;
-    
-    last_redirect = (t_token *)cmd->redirects->get(cmd->redirects, cmd->redirects->size - 1);
-    if (last_redirect->type != HERE_DOC)
-    {
-        close(fd);
-        return ;
-    }
-    if (dup2(fd, STDIN_FILENO) < 0)
-    {
-        close(fd);
-        runtime_err(errno, NULL);
-    }
-    close(fd);
+	t_token		*last_redirect;
+	const int	fd = cmd->fd_here_doc;
+
+	if (cmd->redirects->size == 0 || fd < 0)
+		return ;
+	last_redirect = (t_token *)cmd->redirects->get(cmd->redirects,
+			cmd->redirects->size - 1);
+	if (last_redirect->type != HERE_DOC)
+	{
+		close(fd);
+		return ;
+	}
+	if (dup2(fd, STDIN_FILENO) < 0)
+	{
+		close(fd);
+		runtime_err(errno, NULL);
+	}
+	close(fd);
 }
 
-static void     pipe_redirect(t_cmd *cmd)
+static void	pipe_redirect(t_cmd *cmd)
 {
-    if ((!cmd->is_first_cmd && dup2(cmd->curr_pipe[0], STDIN_FILENO) < 0) ||
-        (!cmd->is_last_cmd && dup2(cmd->next_pipe[1], STDOUT_FILENO) < 0))
-    {
-        close_pipe(cmd->curr_pipe);
-        close_pipe(cmd->next_pipe);
-        if (cmd->fd_here_doc > 0)
-            close(cmd->fd_here_doc);
-        runtime_err(errno, NULL);
-    }
-    close_pipe(cmd->curr_pipe);
-    close_pipe(cmd->next_pipe);
+	if ((!cmd->is_first_cmd && dup2(cmd->curr_pipe[0], STDIN_FILENO) < 0)
+		|| (!cmd->is_last_cmd && dup2(cmd->next_pipe[1], STDOUT_FILENO) < 0))
+	{
+		close_pipe(cmd->curr_pipe);
+		close_pipe(cmd->next_pipe);
+		if (cmd->fd_here_doc > 0)
+			close(cmd->fd_here_doc);
+		runtime_err(errno, NULL);
+	}
+	close_pipe(cmd->curr_pipe);
+	close_pipe(cmd->next_pipe);
 }
