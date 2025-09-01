c8104543eec7f085503d409f74b05e5b107ad4b3
Niklas Raatikainen <nraatika@student.hive.fi>
2025-09-01 14:11:06 +0300

    fixed norm for main.c and shell.c, fixed a leak when exiting minishell via ctrl-D, added setting allocators to NULL after they're destroyed in clean_up()

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
 #include "minishell.h"
+
 volatile sig_atomic_t	g_signal;
 
-int main(int argc, char **args, char **envp)
+int	main(int argc, char **args, char **envp)
 {
+	int	status;
+
 	(void)argc;
 	(void)args;
-
 	init_local_vars(envp);
 	launch_shell();
-	return (get_local_vars()->status);
+	status = get_local_vars()->status;
+	clean_up(true, true);
+	return (status);
 }
