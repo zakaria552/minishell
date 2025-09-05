51c0f89c8e447969b21d3e1c2a2b9657f392d195
Zakaria Farah <zakaria.farah55@outlook.com>
2025-09-05 10:26:49 +0300

    norm fixes

--- src/signal/child_process_signals.c
+++ src/signal/child_process_signals.c
@@ -1,13 +1,13 @@
 #include "minishell.h"
 
-void    handle_child_signal(t_local_vars *vars, bool *signalled, int status)
+void	handle_child_signal(t_local_vars *vars, bool *signalled, int status)
 {
-    if (WTERMSIG(status) == SIGQUIT && !*signalled)
-        reset_readline("Quit. Core dumped", SIGQUIT);
-    else if (WTERMSIG(status) == SIGINT && !*signalled)
-        reset_readline("", SIGINT);
-    else
-        vars->status = WEXITSTATUS(status);
-    *signalled = true;
-    set_handler_to_ignore();
+	if (WTERMSIG(status) == SIGQUIT && !*signalled)
+		reset_readline("Quit. Core dumped", SIGQUIT);
+	else if (WTERMSIG(status) == SIGINT && !*signalled)
+		reset_readline("", SIGINT);
+	else
+		vars->status = WEXITSTATUS(status);
+	*signalled = true;
+	set_handler_to_ignore();
 }
