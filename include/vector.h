f4a408850c1332c48a26a538bcd30fed43664c44
Zakaria Farah <zakaria.farah55@outlook.com>
2025-09-05 11:12:29 +0300

    norm fixes

--- include/vector.h
+++ include/vector.h
@@ -9,28 +9,29 @@ typedef struct s_vector	t_vector;
 
 typedef struct s_vector
 {
-	int		size;
-	int		capacity;
-	size_t	elem_size;
-	void	**data;
-	t_arena	*arena;
-	void	*(*pop)(t_vector *vector);
-	void	*(*push)(t_vector *vector, void *content);
-	void	*(*get)(t_vector *vector, int index);
-	void	(*remove)(t_vector *vector, int index);
-	void	(*free)(t_vector *vector);
-	void	(*clear)(t_vector *vector);
-}	t_vector;
+	int					size;
+	int					capacity;
+	size_t				elem_size;
+	void				**data;
+	t_arena				*arena;
+	void				*(*pop)(t_vector *vector);
+	void				*(*push)(t_vector *vector, void *content);
+	void				*(*get)(t_vector *vector, int index);
+	void				(*remove)(t_vector *vector, int index);
+	void				(*free)(t_vector *vector);
+	void				(*clear)(t_vector *vector);
+}						t_vector;
 
 // Vector initialization
-t_vector	*init_vector(int capacity, void *content, t_arena *arena);
+t_vector				*init_vector(int capacity, void *content,
+							t_arena *arena);
 
 // Vector operation methods
-void		*append(t_vector *vector, void *content);
-void		*get_vector_elem(t_vector *vector, int index);
-void		*pop(t_vector *vector);
-void		free_vector(t_vector *vector);
-void		clear_vector(t_vector *vector);
-void		remove_element(t_vector *vector, int index);
+void					*append(t_vector *vector, void *content);
+void					*get_vector_elem(t_vector *vector, int index);
+void					*pop(t_vector *vector);
+void					free_vector(t_vector *vector);
+void					clear_vector(t_vector *vector);
+void					remove_element(t_vector *vector, int index);
 
 #endif
