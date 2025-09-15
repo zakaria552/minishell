7045b7627da5a8e3c85555908efcee25eb0c2636
Zakaria Farah <zakaria.farah55@outlook.com>
2025-09-15 13:57:44 +0300

    added clean up func for pipeline

--- src/utils/clean_up.c
+++ src/utils/clean_up.c
@@ -6,7 +6,7 @@
 /*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 17:08:07 by zfarah            #+#    #+#             */
-/*   Updated: 2025/09/06 17:14:09 by zfarah           ###   ########.fr       */
+/*   Updated: 2025/09/12 20:18:08 by zfarah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,26 @@ void	clean_up(bool clean_global, bool clean_history)
 
 void	clean_up_and_exit(int err_code)
 {
+	clean_up(true, true);
+	exit(err_code);
+}
+
+void	clean_runtime_err(int err_code, char *msg, int *curr_pipe, int *next_pipe)
+{
+	ft_putstr_fd("minishell: ", 2);
+	if (msg)
+	{
+		ft_putstr_fd(msg, 2);
+		ft_putstr_fd(":", 2);
+	}
+	ft_putstr_fd(strerror(errno), 2);
+	ft_putstr_fd("\n", 2);
+	if (!get_local_vars()->pipeline)
+		return ;
+	if (curr_pipe[0] != -1)
+		close_pipe(curr_pipe);
+	if (next_pipe[0] != -1)
+		close_pipe(next_pipe);
 	clean_up(true, true);
 	exit(err_code);
 }
